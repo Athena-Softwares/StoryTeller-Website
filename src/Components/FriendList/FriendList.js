160908afe80ca4f031abf43ce9cb9ee026b3c27d
Patrick M. De Luca <patrick_mdl@outlook.com>
2024-01-20 18:43:14 -0300

    Atualização da lista de amigos. Ainda falta bastante coisa.

--- src/Components/FriendList/FriendList.js
+++ src/Components/FriendList/FriendList.js
@@ -3,24 +3,40 @@ import { useRouter } from 'next/router';
 import { Container, Grid, Input, Text } from '@nextui-org/react';
 import styles from '../../styles/FriendList.module.css';
 import { amigosTeste } from './dataTeste';
+import Image from "next/image";
+import {Col} from "reactstrap";
 export const FriendList = (props) => {
     const router = useRouter();
     return (
         <Container class={`
             position-absolute end-0 
             border border-1 border-opacity-10
-            col-sm-2
+            col-sm-2 
             ${styles.friends_body}
         `}>
-            <Container class={`${styles.background_header} d-flex justify-content-center align-items-center`}>
-                <Text>Amigos</Text>
+            <Container class={`${styles.friends_header} d-flex justify-content-center align-items-center`}>
+                <Text class={`${styles.friends_header_text}`}>Amigos</Text>
             </Container>
             <Container class={`
+                w-auto 
                 ${styles.friends_body}
             `}>
-                <Input></Input>
+                <div class={`d-flex w-100 justify-content-center p-1`}>
+                    <Input class={`w-100`}/>
+                </div>
                 {amigosTeste.map((info) =>{
-                    return (<Text key={info.Nick}>{info.Nome}</Text>)
+                    return (
+                        <div class={`justify-content-between d-inline d-flex ${styles.friends_card}`} key={info.Nick}>
+                            <Image class={`${styles.friends_card_icon}`} width={50} height={50} src='/foto_perfil.jpg'
+                                   alt='logo Story Teller'/>
+                            <div class={`d-inline justify-content-center`}>
+                                <Text class={`${styles.friends_card_text}`}>{info.Nome}</Text>
+                                <Text class={`${styles.friends_card_text}`}>{info.Nick}</Text>
+                            </div>
+
+                            <span className={`float-end`}>O</span>
+                        </div>
+                    )
                 })}
             </Container>
         </Container>
